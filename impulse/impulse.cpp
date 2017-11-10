beb98c5d310412a19892db6e249e7e3996e5b27c
Chikashi Miyama <chikashimiyama@gmail.com>
2017-11-10 12:32:34 +0100

    impulse signal

--- impulse/impulse.cpp
+++ impulse/impulse.cpp
@@ -14,9 +14,6 @@ public:
 	impulse(float numOutlets);
 
 protected:
-    void m_bang();
-    void m_base_interval(float interval);
-    void m_rand_interval(float interval);
     void m_base_amp(float amp);
     void m_rand_amp(float amp);
 
@@ -35,7 +32,7 @@ private:
     std::default_random_engine re;
 
     unsigned int mOutletCounter;
-    long long mSampleCounter;
+    long mSampleCounter;
 
     FLEXT_CALLBACK(m_bang);
     FLEXT_CALLBACK_F(m_base_interval);
@@ -58,36 +55,19 @@ mSampleCounter(0){
         mNumOutlets = 32;
     }
 
-	AddInAnything("dummy"); 
-    AddInFloat("base interval");
-    AddInFloat("random interval");
+    AddInSignal("base interval");
+    AddInSignal("random interval");
     AddInFloat("base amp");
     AddInFloat("random amp");
 
-    FLEXT_ADDMETHOD(0, m_bang);  
-    FLEXT_ADDMETHOD(1, m_base_interval); 
-    FLEXT_ADDMETHOD(2, m_rand_interval); 
-    FLEXT_ADDMETHOD(3, m_base_amp); 
-    FLEXT_ADDMETHOD(4, m_rand_amp);
+    FLEXT_ADDMETHOD(2, m_base_amp); 
+    FLEXT_ADDMETHOD(3, m_rand_amp);
 
     for(int i = 0 ; i < mNumOutlets; i++){
          AddOutSignal("impulse");
     }
 } 
 
-void impulse::m_bang(){
-    post("base interval: %f samps\nrandom interval: %f samps\n", mBaseInterval, mRandInterval);
-}
-
-void impulse::m_base_interval(float interval){
-    if(interval <= 0 ) interval = 0;
-    mBaseInterval = mstosamps(interval);
-}
-
-void impulse::m_rand_interval(float interval){
-    if(interval <= 0 ) interval = 0;
-    mRandInterval = mstosamps(interval);
-}
 
 void impulse::m_base_amp(float amp){
     mBaseAmp = amp;
@@ -98,6 +78,10 @@ void impulse::m_rand_amp(float amp){
 }
 
 void impulse::m_signal(int n, float *const *in, float *const *out){
+
+    float *baseInterval = in[0];
+    float *randInterval = in[1];
+
     for (int i = 0; i < n; i++){
 
         if(mSampleCounter <= 0){
@@ -111,7 +95,7 @@ void impulse::m_signal(int n, float *const *in, float *const *out){
                 mOutletCounter = 0;
             }
 
-            mSampleCounter = mBaseInterval + random(mRandInterval);
+            mSampleCounter = (long)(mBaseInterval[i] + random(mRandInterval[i]));
 
         }else{
             for(int j = 0; j < mNumOutlets; j++){
